914b5924c84ed06f4bcaca0bad874182529feba8
mokraneghadirbesma <92594692+mokraneghadirbesma@users.noreply.github.com>
2021-10-15 17:03:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+	open modal
+	// $('#open_modal').click(function(){
+		// $('#modal_to_open').css((
+		// 'display':'block'
+		// )
+		// );
+	// });
+	
+// });
+
+function openNav(){
+	document.getElementById("myNav").style.height= "100%";
+}
+
+function closeNav(){
+	document.getElementById("myNav").style.height= "0%";
+}
